52e85a027eb11aeebdaf2ac7ed9ac808cc495472
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-12-11 22:31:30 +0200

    typer

--- tomlcv/__init__.py
+++ tomlcv/__init__.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 from importlib.resources import open_text
 
 import toml
+import typer
 from jinja2 import Environment, StrictUndefined
 
 
@@ -11,27 +12,35 @@ def res2str(name: str):
         return f.read()
 
 
-def format_date(date: date, format="%b %Y"):
-    return date.strftime(format)
+def main(
+        *,
+        in_toml: str = 'resume.toml',
+        out_html: str = 'resume.html',
+        date_format: str = "%b %Y"):
 
-
-if __name__ == "__main__":
-    resume = toml.load('resume.toml')
-    img = resume['basics']['image']
-
-    with open(img, 'rb') as f:
-        resume['basics']['image'] = base64.encodebytes(f.read()).decode()
+    def format_date(date: date):
+        return date.strftime(date_format)
 
     env = Environment(
         undefined=StrictUndefined)
 
     env.filters['date'] = format_date
 
+    resume = toml.load(in_toml)
+    img = resume['basics']['image']
+
+    with open(img, 'rb') as f:
+        resume['basics']['image'] = base64.encodebytes(f.read()).decode()
+
     template = env.from_string(res2str('resume.j2'))
 
     resume_html = template.render(resume)
 
-    with open('resume.html', 'w') as f:
+    with open(out_html, 'w') as f:
         f.write(resume_html)
 
-    print(datetime.now(), 'resume.html created')
+    print(datetime.now(), f'{out_html} created')
+
+
+if __name__ == "__main__":
+    typer.run(main)
