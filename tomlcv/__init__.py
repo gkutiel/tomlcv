88fd10d09bc841c8aecde2a0f6f0f3ac77157df4
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-12-15 14:35:59 +0200

    cv-schema

--- tomlcv/__init__.py
+++ tomlcv/__init__.py
@@ -1,16 +1,47 @@
-import base64
+from dataclasses import dataclass
 from datetime import date
 from importlib.resources import open_text
 from pathlib import Path
 
 import toml
 import typer
+from cattr import Converter
 from jinja2 import Environment, StrictUndefined
+from rich import print
 
 
 def res2str(name: str):
     with open_text('tomlcv', name) as f:
         return f.read()
+
+
+@dataclass
+class contact:
+    email: str
+    phone: str
+    linkedin: str
+    website: str | None = None
+    dblp: str | None = None
+
+
+@dataclass
+class work:
+    startDate: date
+    endDate: date | None
+    employer: str
+    position: str
+    summary: str
+    highlights: list[str]
+
+
+@dataclass
+class cv:
+    image: str
+    name: str
+    title: str
+    summary: str
+    contact: contact
+    work: list[work]
 
 
 def tomlcv(
@@ -26,16 +57,30 @@ def tomlcv(
     def format_date(date: date):
         return date.strftime(date_format)
 
+    def date2str(date: date) -> str:
+        return date.strftime(date_format)
+
+    def date2date(date: date):
+        return date
+
     env = Environment(
         undefined=StrictUndefined)
 
     env.filters['date'] = format_date
 
     resume = toml.load(in_toml)
-    img = resume['basics']['image']
+    print(resume)
 
-    with open(img, 'rb') as f:
-        resume['basics']['image'] = base64.encodebytes(f.read()).decode()
+    con = Converter()
+    con.register_unstructure_hook(date, date2str)
+    con.register_structure_hook(date, lambda d, _: date2date(d))
+
+    resume = con.structure(resume, cv)
+    resume = con.unstructure(resume)
+    # img = resume['basics']['image']
+
+    # with open(img, 'rb') as f:
+    #     resume['basics']['image'] = base64.encodebytes(f.read()).decode()
 
     template = env.from_string(res2str('cv.j2'))
 
